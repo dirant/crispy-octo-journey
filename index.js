1eec42fd13a15d65573c194a9c8f1b61555ebb9e
dirant <dirant11@gmail.com>
2021-03-13 00:18:22 +0100

    Such Irony
    Much wow

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const simpleGit = require('simple-git');
-const git = simpleGit();
-
 const fs = require('fs');
 const uuid = require('uuid');
 
-const testCommit = (ts) => new Promise((res) => {
+const simpleGit = require('simple-git');
+const git = simpleGit();
+
+/**
+ * Generate a commit at timestamp `ts`
+*/
+const doCommit = async (ts) => {
 	fs.writeFileSync('./test.txt', uuid.v4());
-	git.add(['./test.txt']).commit('Comment', {'--date': new Date(ts).toISOString()}, res);
-	console.log(new Date(ts).toISOString());
-});
+	await git.add(['./test.txt']).commit('Comment', {'--date': new Date(ts).toISOString()});
+};
+
+/**
+ * Here's a cookie
+*/
+const attaboy = () => {
+	const rand = Math.random();
+	console.log(
+		rand <= 1/3 ? "You get a commit!"
+		: rand <= 2/3 ? "And YOU get a commit!"
+		: "Everybody gets a commit!"
+	);
+}
 
 (async () => {
-	for (let i = 0; i < 365*10-65; i++) {
-		await testCommit(Date.now() - Math.floor(Math.random() * 365 + 1) * 24*60*60e3)
+	for (let i = 0; i < 365*10; i++) { // Do 10 contributions a day for one year, on average
+		await doCommit(Date.now() - Math.floor(Math.random() * 365 + 1) * 24*60*60e3) // Pick any moment in the last year, even while you were sleeping
+		attaboy(); // that was like, a tough second! Well deserved!
 	}
+	await git.push(); // And now become the guy she tells her bf not to worry about
 })();
